a732af6ca29e3d7749736fd3cc86ddd5292b85f6
EloySPAIN <yole.tres@gmail.com>
2023-05-12 16:07:30 +0200

    a

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,36 +4,69 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    private float velocidad = 5f;
+    public int maxHealth = 5;
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+    public float velocidad = 5f;
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update() {
+        Vector2 position = rigidbody2d.position;
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * velocidad * Time.deltaTime;
-            
+            position.x = position.x + velocidad * Time.deltaTime;
+            rigidbody2d.MovePosition(position);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.right * -velocidad * Time.deltaTime;
-            
+            position.x = position.x - velocidad * Time.deltaTime;
+            rigidbody2d.MovePosition(position);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.up * velocidad * Time.deltaTime;
-
+            position.y = position.y + velocidad * Time.deltaTime;
+            rigidbody2d.MovePosition(position);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.up * -velocidad * Time.deltaTime;
-
+            position.y = position.y - velocidad * Time.deltaTime;
+            rigidbody2d.MovePosition(position);
         }
+
+        //void OnTriggerEnter(Collider other)
+        //{
+        //    if (other.CompareTag("TriggerObject"))
+        //    {
+        //        // Desactiva la colisión del primer collider
+        //        Collider[] colliders = GetComponents<Collider>();
+        //        colliders[1].isTrigger = true;
+        //    }
+        //}
+
+        //void OnTriggerExit(Collider other)
+        //{
+        //    if (other.CompareTag("TriggerObject"))
+        //    {
+        //        // Activa la colisión del primer collider
+        //        Collider[] colliders = GetComponents<Collider>();
+        //        colliders[1].isTrigger = false;
+        //    }
+        //}
     }
-    
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
